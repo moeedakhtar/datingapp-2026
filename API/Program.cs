08f03953e6536589383d83f4171873172b4b99da
Moeed Akhtar <moeedakhter60@gmail.com>
2026-02-26 09:16:20 +0500

    End of Section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,10 @@
+using System.Text;
 using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +16,22 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddCors();
+builder.Services.AddScoped<ITokenService,TokenService>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        var tokenkey = builder.Configuration["TokenKey"] 
+            ?? throw new Exception("Token Key not found - Progrem.cs");
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenkey)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
 // // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 // builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -28,6 +48,9 @@ var app = builder.Build();
 
 app.UseCors(_ => _.AllowAnyHeader().AllowAnyMethod()
 .WithOrigins("http://localhost:4200","https://localhost:4200"));
+
+app.UseAuthentication(); //who are u
+app.UseAuthorization(); //which rights u have
 
 app.MapControllers();
 

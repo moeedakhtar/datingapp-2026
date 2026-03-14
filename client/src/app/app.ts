a8e524f4eeaa6f609d108b0c92fd6cb3a5d6a343
Moeed Akhtar <moeedakhter60@gmail.com>
2026-03-14 10:15:04 +0500

    End Of Section 5

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,22 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
+import { Nav } from "../layout/nav/nav";
+import { AccountService } from '../core/services/account-service';
+import { Home } from "../features/home/home";
+import { User } from '../types/user';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Nav, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App implements OnInit {
-  
+  private accountService = inject(AccountService)
   private http = inject(HttpClient);
   protected readonly title = 'Dating app';
-  protected members = signal<any>([])
+  protected members = signal<User[]>([])
 
   async ngOnInit() {
 
-    this.members.set( await this.getMembwea())
+    this.members.set( await this.getMember());
+    this.setCurrentUser();
 
     // this.http.get('https://localhost:5001/api/members').subscribe({
     //   next: response => this.members.set(response),
@@ -25,9 +30,16 @@ export class App implements OnInit {
     // })
   }
 
-  async getMembwea(){
+  setCurrentUser(){
+    const userString = localStorage.getItem('user');
+    if(!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+  }
+
+  async getMember(){
     try{
-    return lastValueFrom( this.http.get('https://localhost:5001/api/members'))
+    return lastValueFrom( this.http.get<User[]>('https://localhost:5001/api/members'))
     }
     catch(error){
       console.log(error);
